a9c07373e7c4220065f41f253c7b8d224f0ee6e0
Md. Sakib Hasan <rafioar96@gmail.com>
2024-07-26 22:34:29 +0600

    footer responsive layout done

--- src/lib/components/pages/landing/footer/footer.svelte
+++ src/lib/components/pages/landing/footer/footer.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
-export let footerData;
-console.log(footerData);
-
+  export let footerData;
+  console.log(footerData);
 </script>
+
+<div
+  class="flex w-full flex-col justify-center bg-MineShaftV2/25 px-[0.938rem] py-[4rem] md:px-[2.5rem] md:py-[7.5rem] xl:px-[20rem]">
+  <div class=" flex flex-auto flex-wrap justify-between">
+    <div
+      class="optionDiv max-w-full text-[3.55rem] font-bold leading-[3.5rem] md:max-w-[16.5rem] md:text-[5rem] md:leading-[5rem]">
+      drop us a line
+    </div>
+    <div class="flex flex-auto flex-wrap justify-end">
+      <div class="optionDiv">asdfj</div>
+      <div class="optionDiv">djhsajh</div>
+      <div class="optionDiv">djhsajh</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .optionDiv {
+    @apply pl-[2rem] md:pl-[3rem];
+  }
+</style>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { urlFor } from '$lib/sanity/sanity-client';
   import type { LayoutData } from './$types';
-  import Navbar from "../../lib/components/pages/landing/hero/Navbar.svelte";
+  import Navbar from '../../lib/components/pages/landing/hero/Navbar.svelte';
   import Footer from '$lib/components/pages/landing/footer/footer.svelte';
   export let data: LayoutData;
   $: ({
@@ -13,20 +13,19 @@
   let faviconImage = favicon
     ? urlFor(favicon).size(256, 256).ignoreImageParams().url()
     : null;
-    $:console.log(data)
+  $: console.log(data);
 </script>
 
 <svelte:head>
   <link rel="icon" type="image/png" href={faviconImage} />
 </svelte:head>
 
-<main class="overflow-hidden">
-
-</main>
-<div class="w-screen min-h-[3.375rem] md:min-h-[6.5rem]">
-  <Navbar/>
+<main class="overflow-hidden"></main>
+<div class="min-h-[3.375rem] w-screen md:min-h-[6.5rem]">
+  <Navbar />
 </div>
 
-
 <slot />
-<Footer footerData={data.site.footer}/>
+<div class="w-screen ">
+  <Footer footerData={data.site.footer} />
+</div>
