bded0c0f36e26a1c756e4d4c49a486c8f0448ced
Md. Sakib Hasan <rafioar96@gmail.com>
2024-07-28 12:58:55 +0600

    Added Lucisa icon Library and Down Right arrow icon

--- src/lib/components/pages/landing/footer/footer.svelte
+++ src/lib/components/pages/landing/footer/footer.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import type { Footer } from "$lib/types/Footer";
-  import FooterCard from "./footerCard.svelte";
-
-  export let footerData:Footer;
-  console.log(footerData);
+  import type { Footer } from '$lib/types/Footer';
+  import FooterCard from './footerCard.svelte';
+  import { CornerDownRight } from 'lucide-svelte';
+  export let footerData: Footer;
 </script>
 
 <div
@@ -12,16 +11,19 @@
     <div
       class="optionDiv max-w-[22.5rem] text-[3.55rem] font-bold leading-[3.5rem] md:max-w-[16.5rem] md:text-[5rem] md:leading-[5rem]">
       {footerData.title}
-      <i class="fa-sharp fa-solid fa-turn-up rotate-90 text-[2.55rem] md:text-[3.55rem]"></i>
+      <CornerDownRight size=64/>
     </div>
     <div class="flex flex-auto flex-wrap">
-        <FooterCard optionTitle="" options={footerData.sections.section}/>
-        <FooterCard optionTitle={footerData.social.title} options={footerData.social.socials}/>
-        <FooterCard optionTitle="" options={footerData.portfolios.portfolios}/>
+      <FooterCard optionTitle="" options={footerData.sections.section} />
+      <FooterCard
+        optionTitle={footerData.social.title}
+        options={footerData.social.socials} />
+      <FooterCard optionTitle="" options={footerData.portfolios.portfolios} />
     </div>
   </div>
-  <div class=" flex flex-col md:flex-row justify-start md:justify-center  pt-[2rem] md:pt-5">
-    {#each footerData.CopyWriteInfo as info }
+  <div
+    class=" flex flex-col justify-start pt-[2rem] md:flex-row md:justify-center md:pt-5">
+    {#each footerData.CopyWriteInfo as info}
       <h1 class="pl-[2rem]">{info}</h1>
     {/each}
   </div>
@@ -32,4 +34,3 @@
     @apply pl-[2rem] md:pl-[3rem];
   }
 </style>
-
