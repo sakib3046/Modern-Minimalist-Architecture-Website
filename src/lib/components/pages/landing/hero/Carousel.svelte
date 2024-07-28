45142c5aa16f63400b535e898db8edb6fe8e8901
Md. Sakib Hasan <rafioar96@gmail.com>
2024-07-28 12:47:33 +0600

    Carousel Slider Number button Added

--- src/lib/components/pages/landing/hero/Carousel.svelte
+++ src/lib/components/pages/landing/hero/Carousel.svelte
@@ -1,21 +1,50 @@
-<script>
-  import emblaCarouselSvelte from 'embla-carousel-svelte';
+<script lang="ts">
+  import emblaCarouselSvelte, {
+    type EmblaCarouselType,
+    type EmblaOptionsType,
+    type EmblaPluginType,
+  } from 'embla-carousel-svelte';
   import Autoplay from 'embla-carousel-autoplay';
   import SanityImage from '$lib/sanity/sanity-image/sanity-image.svelte';
   import { imgBuilder } from '$lib/sanity/sanity-client';
+  import DotButton from './DotButton.svelte';
 
   export let carouselImages;
   export let sliderImageText;
+
+  let emblaApi: EmblaCarouselType;
+  let sectionEl: HTMLElement;
+  let selectedIndex = 0;
+  let scrollSnaps: number[];
+  let isIntersecting = false;
+  export let plugins: EmblaPluginType[] = [Autoplay()];
+  export let options: Partial<EmblaOptionsType> = { loop: false };
+
+  const onInit = (event: CustomEvent<EmblaCarouselType>) => {
+    emblaApi = event.detail;
+    scrollSnaps = event.detail.scrollSnapList();
+    selectedIndex = event.detail.selectedScrollSnap();
+  };
+
+  $: if (emblaApi) {
+    emblaApi.on('select', ({ selectedScrollSnap, scrollSnapList }) => {
+      selectedIndex = selectedScrollSnap();
+      scrollSnaps = scrollSnapList();
+    });
+  }
+
   // console.log(carouselImages);
-  let options = { loop: false };
-  let plugins = [Autoplay()];
 </script>
 
-<div class="embla relative" use:emblaCarouselSvelte={{ options, plugins }}>
-  <div class="embla__container ">
+<div
+  class="embla relative"
+  use:emblaCarouselSvelte={{ options, plugins }}
+  on:emblaInit={onInit}>
+  <div class="embla__container">
     {#each carouselImages as carImg}
-      <div class="embla__slide w-[22.5rem] h-[36.074rem] md:w-[59rem] 
-      xl:w-[75rem] xl:h-[47.313rem]">
+      <div
+        class="embla__slide h-[36.074rem] w-[22.5rem] md:w-[59rem]
+      xl:h-[47.313rem] xl:w-[75rem]">
         <SanityImage
           lqip
           class="h-full w-full object-cover"
@@ -26,10 +55,27 @@
       </div>
     {/each}
   </div>
-  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <h1 class="text-white text-[2.5rem] md:text-[5rem] font-[400]">{sliderImageText}</h1>
+  <div
+    class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+    <h1 class="text-[2.5rem] font-[400] text-white md:text-[5rem]">
+      {sliderImageText}
+    </h1>
   </div>
 </div>
+{#if !!scrollSnaps?.length}
+  <nav class=" flex items-center justify-center pt-[32px]">
+    <div class="flex space-x-[8px]">
+      {#each scrollSnaps as _, index}
+        <DotButton
+        slideNumber={index+1}
+          active={index === selectedIndex}
+          clickAction={() => {
+            emblaApi.scrollTo(index);
+          }} />
+      {/each}
+    </div>
+  </nav>
+{/if}
 
 <style>
   .embla {
