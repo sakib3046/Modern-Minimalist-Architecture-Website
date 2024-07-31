a5a7ec34c5b2abf45e7644b0e948c990a7edaae6
Md. Sakib Hasan <rafioar96@gmail.com>
2024-08-01 00:02:28 +0600

    Another try for footer layout fixing

--- src/lib/components/pages/landing/footer/footer.svelte
+++ src/lib/components/pages/landing/footer/footer.svelte
@@ -9,7 +9,7 @@
   class="flex w-full flex-col justify-center bg-Alabster px-[0.938rem] py-[4rem] md:px-[2.5rem] md:py-[7.5rem] xl:px-[20rem]">
   <div class=" md:flex md:flex-row md:flex-wrap  md:justify-center grid grid-cols-2">
       <div
-        class="optionDiv flex flex-col gap-[1.25rem] justify-start self-stretch text-[3.55rem] font-bold leading-[3.5rem] md:max-w-[16.5rem] md:text-[5rem] md:leading-[5rem] mr-[10rem] col-span-2">
+        class="optionDiv flex flex-col gap-[1.25rem] justify-start self-stretch text-[3.55rem] font-bold leading-[3.5rem] md:max-w-[16.5rem] md:text-[5rem] md:leading-[5rem] mr-[19.5rem] col-span-2">
         <div class="hidden md:block md:h-[6.25rem] xl:hidden"></div>
         {footerData.title}
         <CornerDownRight size=64/>
